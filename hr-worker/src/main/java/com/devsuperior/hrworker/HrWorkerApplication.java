e661765937bbdf882192c0fc2fe5d9458b4d9ec4
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-07-01 20:16:21 +0100

    CommandLineRunner implemented

--- hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
@@ -1,15 +1,27 @@
 package com.devsuperior.hrworker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
 @EnableEurekaClient
 @SpringBootApplication
-public class HrWorkerApplication {
+public class HrWorkerApplication implements CommandLineRunner{
 
+	@Value("${profile.name}")
+	private String profileName;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrWorkerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println("Current profile: "+profileName);
 	}
 
 }
