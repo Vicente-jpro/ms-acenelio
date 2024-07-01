29274e46631b88a671cb7a843769adb8648f986c
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-07-01 21:58:05 +0100

    Profile description added

--- hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
@@ -1,6 +1,7 @@
 package com.devsuperior.hruser;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,12 +15,16 @@ public class HrUserApplication implements CommandLineRunner {
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 	
+	@Value("${profile.name}")
+	private String profileName;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		System.out.println("Profile name: "+profileName);
 		System.out.println("BCRYPT = " + passwordEncoder.encode("123456"));
 	}
 }
